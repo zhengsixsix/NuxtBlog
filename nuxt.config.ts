b381f8ce7fb6dd34bad5676b704d974f959abbd6
1314520zjX! <1833496831@qq.com>
2023-04-04 16:25:49 +0800

    feat: 增加首页静态页面

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
+/*
+ * @Author: 1314520zjX! 1833496831@qq.com
+ * @Date: 2023-04-04 10:08:28
+ * @LastEditors: 1314520zjX! 1833496831@qq.com
+ * @LastEditTime: 2023-04-04 16:12:54
+ * @FilePath: \blog\nuxt.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import path from 'path'
 import { defineNuxtConfig } from 'nuxt/config'
-import eslint from 'vite-plugin-eslint'
 
 export default defineNuxtConfig({
   nitro: {
@@ -19,10 +26,6 @@ export default defineNuxtConfig({
     '@': path.resolve(__dirname)
   },
   modules: [
-    '@vueuse/nuxt',
     '@element-plus/nuxt'
   ],
-  vite: {
-    plugins: [eslint()]
-  }
 })
